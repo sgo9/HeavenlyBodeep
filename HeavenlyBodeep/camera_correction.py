0cd4653d84e3539f108b59111a90d7f5d686801a
anais giustiniani <anais.giusti@gmail.com>
2022-03-01 14:47:27 +0100

    new files for camera rotation

---
+++ HeavenlyBodeep/camera_correction.py
@@ -0,0 +1,24 @@
+import pyautogui
+import time
+from PIL import Image
+import cv2
+import numpy as np
+from tensorflow import keras
+import os
+import sys
+# while True:
+#     time.sleep(2.0)
+
+path=os.path.join(os.path.dirname(__file__),'model','rotation_model')
+
+print(path)
+model = keras.models.load_model(path)
+
+def get_angle(model):
+    ''' Takes screenshot and returns angular position '''
+    img = pyautogui.screenshot()
+    img=img.crop((660,240,1260,840)).resize((64,64))
+    img_pred=np.array([np.array(img)[:,:,0:3]])
+    pred=np.argmax(model.predict(img_pred), axis=-1)[0]*5
+    print(pred)
+    return pred
