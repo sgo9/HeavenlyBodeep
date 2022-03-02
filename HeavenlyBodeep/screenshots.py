e030a837862b32acb40c600aa35e8503d4a97e2c
Arthur DIESBECQ <arthurdiesbecq@gmail.com>
2022-03-02 08:48:59 +0100

    new main with screenshot option

--- HeavenlyBodeep/screenshots.py
+++ HeavenlyBodeep/screenshots.py
@@ -2,30 +2,29 @@ import pyautogui
 from pynput.keyboard import Key, Controller
 import os
 import time
+from datetime import datetime,timedelta
 
 
 path=os.path.join(os.path.dirname(os.path.dirname(__file__)),'raw_data')
 
 def make_screenshot(file_name):
-    keyboard = Controller()
-    key = "c"
-    time.sleep(0.3)
-    keyboard.press(key)
-    keyboard.release(key)
     pyautogui.screenshot(os.path.join(path,f'{file_name}.png'))
 
 
 
-i=0
-while True:
-    if i==0:
-        time.sleep(10)
-        i+=1
-    else:
-        time.sleep(1)
-        make_screenshot(i)
-        i+=1
-
-
-
-
+# i=0
+# while True:
+#     if i==0:
+#         time.sleep(10)
+#         i+=1
+#     else:
+#         time.sleep(1)
+#         make_screenshot(i)
+#         i+=1
+#new_now=now+timedelta(seconds=5)
+# now = datetime.now().second
+# while True:
+    
+#     print(now-datetime.now().second)
+    
+    
