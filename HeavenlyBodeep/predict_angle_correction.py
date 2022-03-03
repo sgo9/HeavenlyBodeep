280c5b9fcaf5b92e0f5407f39ca064135ff901cd
Simon Gousset <simon.gousset@gmail.com>
2022-03-03 10:45:25 +0100

    station distance detection

---
+++ HeavenlyBodeep/predict_angle_correction.py
@@ -0,0 +1,5 @@
+"""Use astronaut angle detection trained model to predict the camera correction"""
+
+def compute_angle_correction(game_image):
+    """Return the astronaut angle in radiant considering an image of the game"""
+    return 10/180*3.14 # TODO write the real function
