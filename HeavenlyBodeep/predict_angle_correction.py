7e7dee942e9062092ed63d66309d0c9878e724dd
Simon Gousset <simon.gousset@gmail.com>
2022-03-03 17:14:25 +0100

    no change

--- HeavenlyBodeep/predict_angle_correction.py
+++ HeavenlyBodeep/predict_angle_correction.py
@@ -1,5 +1,5 @@
 """Use astronaut angle detection trained model to predict the camera correction"""
 
 def compute_angle_correction(game_image):
-    """Return the astronaut angle in radiant considering an image of the game"""
+    """Return the astronaut angle in radiant considering an image of the game [-pi;+pi]"""
     return 10/180*3.14 # TODO write the real function
