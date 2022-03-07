614a02ae8271c0fdfeb7a67ee0f26afbe913cb29
anais giustiniani <anais.giusti@gmail.com>
2022-03-07 11:31:48 +0100

    do_action function in Agent class

--- AstroBot/agent.py
+++ AstroBot/agent.py
@@ -1,15 +1,17 @@
 
 import random
 import numpy as np
+from HeavenlyBodeep.deep_controller import update_vjoy
+from AstroBot.action_space import generate_movement_dict
 
-from model import model_linear
 
 class Agent:
 
     def __init__(self,model):
         self.n_moves=0
         self.epsilon=0 #randomness
-        self.model=model_linear() 
+        self.model=model #ToDo create class model
+        self.move_dict = generate_movement_dict(100)
         #self.trainer=QTrainer(self.model,lr=LR,gamma=self.gamma) 
         pass
 
@@ -19,13 +21,17 @@ class Agent:
 
     def get_action(self,state):
         #random moves:tradeoff exploration/exploitation
-        self.epsilon=20-self.n_moves  #TODO validate how to manage epsilon
+        self.epsilon=20-self.n_moves
         if random.randint(0,200)<self.epsilon:
             move=random.randint(0,2)
         else:
-            prediction = self.model.predict(state)
-            #TODO move=prediction.argmax(prediction).item()
+            prediction = self.model(state)
+            move=self.model.argmax(prediction).item()
         return move
+    
+    def do_action(self,action,j,angle_correction):
+        for player_position in self.move_dict[action]:
+            update_vjoy(j, player_position, {}, angle_correction) 
 
     def remember(self,state,action,reward,next_state,done):
         self.memory.append((state,action,reward,next_state,done))#popleft if maxmemory is reach
