0ff7d033a0e8bd280d9c43f1a9b3e0c8a1799c4e
Arthur DIESBECQ <arthurdiesbecq@gmail.com>
2022-03-07 09:15:42 +0100

    update

--- AstroBot/agent.py
+++ AstroBot/agent.py
@@ -1,13 +1,14 @@
 
 import random
 
+from model import model_linear
 
 class Agent:
 
     def __init__(self,model):
         self.n_moves=0
         self.epsilon=0 #randomness
-        self.model=model #ToDo create class model
+        self.model=model_linear() 
         #self.trainer=QTrainer(self.model,lr=LR,gamma=self.gamma) 
         pass
 
@@ -17,12 +18,12 @@ class Agent:
 
     def get_action(self,state):
         #random moves:tradeoff exploration/exploitation
-        self.epsilon=20-self.n_moves
+        self.epsilon=20-self.n_moves  #TODO validate how to manage epsilon
         if random.randint(0,200)<self.epsilon:
             move=random.randint(0,2)
         else:
-            prediction = self.model(state)
-            move=model.argmax(prediction).item()
+            prediction = self.model.predict(state)
+            #TODO move=prediction.argmax(prediction).item()
         return move
 
         return
