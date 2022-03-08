ada5563a920bd058b1d6aa218cb628726e298529
Simon Gousset <simon.gousset@gmail.com>
2022-03-08 15:04:35 +0100

    dummy bot

--- AstroBot/get_train_data.py
+++ AstroBot/get_train_data.py
@@ -1,16 +1,17 @@
-import os
+import numpy as np
 import pandas as pd
-import random
-from AstroBot.action_space import generate_movement_dict
-from HeavenlyBodeep.deep_controller import update_vjoy
-from ImageProcessing.station_detection import station_polar_coordinates
-from HeavenlyBodeep.predict_angle_correction import compute_angle_correction
+import os
 import pyvjoy
 from datetime import datetime
 import pyautogui
 import cv2
-import numpy as np
 from time import sleep
+
+from AstroBot.action_space import generate_movement_dict
+from AstroBot.dummy_bot import dummy_decision
+from HeavenlyBodeep.deep_controller import update_vjoy
+from ImageProcessing.station_detection import station_polar_coordinates
+from HeavenlyBodeep.predict_angle_correction import compute_angle_correction
 
 # Ignoring tensorflow loading warnings (CUDA)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
@@ -19,55 +20,64 @@ from tensorflow.keras import models
 # IgnoringStop ignoring tensorflow loading warnings (CUDA)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0' 
 
-# For VJoy output:
-j = pyvjoy.VJoyDevice(1)
-
-#importing model for angle correction:
-model_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'model.h5')
-model = models.load_model(model_path)
 
 
-movement_dict = generate_movement_dict(1)
-
-def execute_movement(move, angle_correction):
+def execute_movement(j, move, angle_correction):
     for player_position in move:
         update_vjoy(j, player_position, {}, angle_correction)
         sleep(0.1)
 
-log_dict = {'Id':[], 'Time': [], 'Move':[], 'Station_Distance': [], 'Station_Angle':[], 'Astronaut_Angle':[]}
+def train_dummy_bot(move_number):
 
-training_time = 20
+    # For VJoy output:
+    j = pyvjoy.VJoyDevice(1)
 
-for _ in range(training_time):
+    #importing model for angle correction:
+    model_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'model.h5')
+    model = models.load_model(model_path)
 
+    # importing movement dictionnary
+    movement_dict = generate_movement_dict()
 
-    # environment
-    game_image = pyautogui.screenshot()
-    
-    # angle correction
-    angle_correction = compute_angle_correction(game_image, model)
+    log_dict = {'Id':[], 'Time': [], 'Move':[], 'Station_Distance': [], 'Station_Angle':[], 'Astronaut_Angle':[]}
+    station_angle_memory = 0
 
-    # action
-    move_key = random.randint(0,2)
-    move = movement_dict[move_key]
-    execute_movement(move, angle_correction)
+    for _ in range(move_number):
 
-    # observations
-    log_dict['Id'].append(_)
-    log_dict['Time'].append(datetime.now()) # TODO try DateTime.Now.ToShortTimeString()
-    log_dict['Move'].append(move_key)
-    game_image = cv2.cvtColor(np.array(game_image), cv2.COLOR_RGB2BGR) # TODO remove this line -- integrated in ImageProcessing
-    distance, angle = station_polar_coordinates(game_image)
-    log_dict['Station_Distance'].append(distance)
-    log_dict['Station_Angle'].append(angle)
-    log_dict['Astronaut_Angle'].append(angle_correction)
-    sleep(1)
-    print(datetime.now(), move_key, distance, angle)
+        # environment
+        game_image = pyautogui.screenshot()
+        
+        # observation
+        astronaut_angle = compute_angle_correction(game_image, model)
+        game_image = cv2.cvtColor(np.array(game_image), cv2.COLOR_RGB2BGR) # TODO remove this line -- integrated in ImageProcessing
+        station_distance, station_angle = station_polar_coordinates(game_image)
 
-print(log_dict)
-df = pd.DataFrame(log_dict)
-df.to_csv('log_train.csv', index=False)
+        # station angle memory
+        if station_angle == None:
+            station_angle = station_angle_memory
+        station_angle_memory = station_angle
 
+        # action with dummy_bot
+        move_key = dummy_decision(station_distance, station_angle, astronaut_angle)
 
+        move = movement_dict[move_key]
+        execute_movement(j, move, astronaut_angle)
+
+        # log
+        log_dict['Id'].append(_)
+        log_dict['Time'].append(datetime.now()) # TODO try DateTime.Now.ToShortTimeString()
+        log_dict['Move'].append(move_key)
+
+        log_dict['Station_Distance'].append(station_distance)
+        log_dict['Station_Angle'].append(station_angle)
+        log_dict['Astronaut_Angle'].append(astronaut_angle)
+        sleep(1)
+        print(move_key, station_distance, int(station_angle*180/np.pi), int(astronaut_angle*180/np.pi))
+
+    df = pd.DataFrame(log_dict)
+    df.to_csv('log_train.csv', index=False)
+
+if __name__=="__main__":
+    train_dummy_bot(200)
 
 
