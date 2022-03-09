9875f1b1e44469028c5bf45e86d4ecc83edc9a66
Arthur DIESBECQ <arthurdiesbecq@gmail.com>
2022-03-09 10:09:56 +0100

    threshold movement

--- HeavenlyBodeep/deep_controller_class.py
+++ HeavenlyBodeep/deep_controller_class.py
@@ -21,6 +21,8 @@ class DeepController():
         self.left_hand_coordinate_y=int(0x4000)
         self.right_hand_coordinate_x=int(0x4000)
         self.right_hand_coordinate_y=int(0x4000)
+        self.threshold_movement=3000
+        self.i=0
 
     def compute_buttons_value(self,a_command, grab_left, grab_right, legs_status, reset_camera):
         """Return the decimal value corresponding to the binary mapping of buttons status
@@ -35,35 +37,40 @@ class DeepController():
         if player_position:
 
             # joystick position (hands distance and position)
-            new_left_hand_coordinate_x = int(0x4000)-(player_position['left_hand_dist'] * sin(player_position['left_hand_angle']-angle_correction))*int(0x4000)
-            new_left_hand_coordinate_y = (player_position['left_hand_dist'] * -cos(player_position['left_hand_angle']-angle_correction))*int(0x4000)+int(0x4000)
-            new_left_hand_coordinate_x=coordinate_correction(new_left_hand_coordinate_x)
-            new_left_hand_coordinate_y=coordinate_correction(new_left_hand_coordinate_y)
-
-            #Get difference of postion
-            new_left_hand_coordinate_x-=self.left_hand_coordinate_x
-            new_left_hand_coordinate_y-=self.left_hand_coordinate_y
-
-            #No negative numbers, environment is between 0,8000
-            self.left_hand_coordinate_x=coordinate_correction(int(0x4000)-new_left_hand_coordinate_x)
-            self.left_hand_coordinate_y =coordinate_correction(int(0x4000)-new_left_hand_coordinate_y)
-            self.j.data.wAxisX = self.left_hand_coordinate_x
-            self.j.data.wAxisY = self.left_hand_coordinate_y
-
-
-            new_right_hand_coordinate_x = int(0x4000)+(player_position['right_hand_dist'] * sin(player_position['right_hand_angle']+angle_correction))*int(0x4000)
-            new_right_hand_coordinate_y = (player_position['right_hand_dist'] * -cos(player_position['right_hand_angle']+angle_correction))*int(0x4000)+int(0x4000)
-            new_right_hand_coordinate_x=coordinate_correction(new_right_hand_coordinate_x)
-            new_right_hand_coordinate_y=coordinate_correction(new_right_hand_coordinate_y)
-            new_right_hand_coordinate_x-=self.right_hand_coordinate_x
-            new_right_hand_coordinate_y-=self.right_hand_coordinate_y
-
-            self.right_hand_coordinate_x = coordinate_correction(int(0x4000)-new_right_hand_coordinate_x)
-            self.right_hand_coordinate_y = coordinate_correction(int(0x4000)-new_right_hand_coordinate_y)
+            new_left_hand_coordinate_x =coordinate_correction(int(0x4000)-(player_position['left_hand_dist'] * sin(player_position['left_hand_angle']-angle_correction))*int(0x4000))
+            new_left_hand_coordinate_y =coordinate_correction((player_position['left_hand_dist'] * -cos(player_position['left_hand_angle']-angle_correction))*int(0x4000)+int(0x4000))
             
-            self.j.data.wAxisYRot = self.right_hand_coordinate_x
-            self.j.data.wAxisXRot = self.right_hand_coordinate_y
+            if abs(new_left_hand_coordinate_x-self.left_hand_coordinate_x)<self.threshold_movement:
+                self.left_hand_coordinate_x=int(0x4000)
+            else:
+                self.left_hand_coordinate_x=new_left_hand_coordinate_x
+            
+            if abs(new_left_hand_coordinate_y-self.left_hand_coordinate_y)<self.threshold_movement:
+                #self.left_hand_coordinate_y=int(0x4000)
+                self.j.data.wAxisX = int(0x4000)
+                self.j.data.wAxisY = int(0x4000)
+            else:
+                self.left_hand_coordinate_y=new_left_hand_coordinate_y
+                self.j.data.wAxisX = self.left_hand_coordinate_x
+                self.j.data.wAxisY = self.left_hand_coordinate_y
 
+            new_right_hand_coordinate_x=coordinate_correction(int(0x4000)+(player_position['right_hand_dist'] * sin(player_position['right_hand_angle']+angle_correction))*int(0x4000))
+            new_right_hand_coordinate_y =coordinate_correction((player_position['right_hand_dist'] * -cos(player_position['right_hand_angle']+angle_correction))*int(0x4000)+int(0x4000))
+
+            if abs(new_right_hand_coordinate_x-self.right_hand_coordinate_x)<self.threshold_movement:
+                self.right_hand_coordinate_x=int(0x4000)
+            else:
+                self.right_hand_coordinate_x=new_right_hand_coordinate_x
+            
+            if abs(new_right_hand_coordinate_y-self.right_hand_coordinate_y)<self.threshold_movement:
+                #self.right_hand_coordinate_y=int(0x4000)
+                self.j.data.wAxisYRot = int(0x4000)
+                self.j.data.wAxisXRot = int(0x4000)  
+            else:
+                self.right_hand_coordinate_y=new_right_hand_coordinate_y
+                self.j.data.wAxisYRot = self.right_hand_coordinate_x
+                self.j.data.wAxisXRot = self.right_hand_coordinate_y         
+            
             
             # buttons status (A, left and right grab, legs status)
             a_command = player_position.get('a_command', False)
