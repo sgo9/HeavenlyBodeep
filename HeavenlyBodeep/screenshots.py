4ec83e0e02c73a247d2cb3fc5c03dc9262f9d8ee
Jeremy <jmarchand1@gmail.com>
2022-03-09 10:09:23 +0100

    updating screenshots

--- HeavenlyBodeep/screenshots.py
+++ HeavenlyBodeep/screenshots.py
@@ -9,24 +9,26 @@ from inputs import get_gamepad
 
 
 path=os.path.join(os.path.dirname(os.path.dirname(__file__)),'raw_data')
+print(path)
 
 def make_screenshot(file_name):
     pyautogui.screenshot(os.path.join(path,f'{file_name}.png'))
 
-
-i=max([0]+[int(i[:-4]) for i in os.listdir(path) if i[-4:]==".png"])+1
-time.sleep(5)
-while True:
-    make_screenshot(i)
-    time.sleep(0.5)
-    i += 1
-
-
-
+if __name__ == "__main__":
+    i=max([0]+[int(i[:-4]) for i in os.listdir(path) if i[-4:]==".png"])+1
+    print(i)
+    time.sleep(5)
+    while True:
+        make_screenshot(i)
+        time.sleep(0.5)
+        i += 1
 
 
-i=max([int(i[:-4]) for i in os.listdir(path) if i[-4:]==".png"])+1
-print(i)
+
+
+
+# i=max([int(i[:-4]) for i in os.listdir(path) if i[-4:]==".png"])+1
+# print(i)
 # while True:
 #     if i==0:
 #         time.sleep(10)
